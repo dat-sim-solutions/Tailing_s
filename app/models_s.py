c741ac89fd6d9575004f4036e6498e47fd101e36
dat-sim-solutions <75774314+dat-sim-solutions@users.noreply.github.com>
2026-03-22 17:41:14 -0500

    Update models_s.py

--- app/models_s.py
+++ app/models_s.py
@@ -19,7 +19,7 @@ def calculate_slope_stability(xc, yc, R, sensor_u_kpa, kh=0.0, gamma=18, gamma_w
         return np.sqrt(max(0, k * (x - x_toe))) + y_toe
 
     # 2. FIND INTERSECTIONS
-    x_scan = np.linspace(xc - R + 0.01, xc + R - 0.01, 1000) # 500
+    x_scan = np.linspace(xc - R + 0.01, xc + R - 0.01, 2000) # 500
     y_dam_scan = np.interp(x_scan, dx, dy)
     y_circ_scan = yc - np.sqrt(R**2 - (x_scan - xc)**2) # y_circ_scan as a function fo x_scan
     
@@ -27,7 +27,7 @@ def calculate_slope_stability(xc, yc, R, sensor_u_kpa, kh=0.0, gamma=18, gamma_w
     sign_changes = np.where(np.diff(np.sign(diff)))[0]
     
     if len(sign_changes) < 2:
-        return None, None, None
+        return None, None, (None, None)
     
     x_start, x_end = x_scan[sign_changes[0]], x_scan[sign_changes[-1]]
     
