5a1b5c7b768ce11d0c98e0ae874946419c9cf49c
dat-sim-solutions <75774314+dat-sim-solutions@users.noreply.github.com>
2026-04-05 19:36:44 -0500

    Update streamlit_s_app.py

--- streamlit_s_app.py
+++ streamlit_s_app.py
@@ -192,7 +192,7 @@ if not data.empty:
                 st.success(f"✅ **Safe:** The most critical center found has an FS of: **{min_found:.3f}**")
 
     # with st.expander("📈 View Solver Convergence"):
-    st.write("📈 View Solver Convergence")
+    st.write("### 📈 Solver Convergence")
     fig_conv, ax_conv = plt.subplots(figsize=(6, 2))
     ax_conv.plot(history, marker='o', linestyle='-', color='purple')
     ax_conv.set_title("Bishop Iteration Path")
